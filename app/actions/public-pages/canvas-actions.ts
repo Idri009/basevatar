df8184e56bafedd79534ce5ea075f560863c89b8
enesyukselx <enesyuksel9903@gmail.com>
2024-06-19 23:33:08 +0300

    refactor: improve image upload process

--- app/actions/public-pages/canvas-actions.ts
+++ app/actions/public-pages/canvas-actions.ts
@@ -7,32 +7,42 @@ import { getSession } from "@/app/utils/sessionHelpers";
 import { postNewSavedImage } from "@/app/utils/slackHelpers";
 
 export async function uploadImageToServer(data: string) {
-    const base64Image = data.split(";base64,").pop();
-    if (!base64Image) return;
-    const settings = await prisma.settings.findMany();
-    const day = settings.find((setting) => setting.key === "day");
-    const session = await getSession();
-    if (!session || !day) return;
-    const userAddres = session.address;
-    //create day folder
-    const dayFolder = path.join(process.cwd(), "public", "images", day.value);
-    await fs.mkdir(dayFolder, { recursive: true });
-    const imagePath = path.join(process.cwd(), "public", "images", day.value, `${uuidv4()}.jpg`);
-    await fs.writeFile(imagePath, base64Image, "base64");
-    const url = `/images/${day.value}/${path.basename(imagePath)}`;
+    try {
+        const base64Image = data.split(";base64,").pop();
+        if (!base64Image) return;
+        const settings = await prisma.settings.findMany();
+        const day = settings.find((setting) => setting.key === "day");
+        const session = await getSession();
+        if (!session || !day) return;
+        const userAddress = session.address;
+        //create day folder
+        const dayFolder = path.join(process.cwd(), "public", "images", day.value);
+        await fs.mkdir(dayFolder, { recursive: true });
+        const imagePath = path.join(process.cwd(), "public", "images", day.value, `${uuidv4()}.jpg`);
+        await fs.writeFile(imagePath, base64Image, "base64");
+        const url = `/images/${day.value}/${path.basename(imagePath)}`;
 
-    //save image to db
-    await prisma.images.create({
-        data: {
-            day: +day.value,
-            url,
-            address: userAddres,
-            isSelected: true,
-        },
-    });
+        await saveToDatabase(+day.value, url, userAddress);
+        //slack message
+        await postNewSavedImage(process.env.NEXT_PUBLIC_BASE_URL + url, userAddress, day.value);
 
-    //slack message
-    await postNewSavedImage(process.env.NEXT_PUBLIC_BASE_URL + url, userAddres, day.value);
-
-    return url;
+        return url;
+    } catch (e) {
+        console.error(e);
+    }
 }
+
+const saveToDatabase = async (day: number, url: string, address: string, isSelected: boolean = true) => {
+    try {
+        await prisma.images.create({
+            data: {
+                day,
+                url,
+                address,
+                isSelected,
+            },
+        });
+    } catch (e) {
+        console.error(e);
+    }
+};
