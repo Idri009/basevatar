37abb8109d23ef27726aa15a0d3652a84ae7358d
enesyukselx <enesyuksel9903@gmail.com>
2024-10-21 10:41:28 +0300

    feat: add global type declaration for environment variables

---
+++ globals.d.ts
@@ -0,0 +1,23 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        SESSION_SECRET: string;
+        NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: string;
+        DATABASE_URL: string;
+        ADMIN_WALLET_ADDRESS: string;
+        NEXT_PUBLIC_CONTRACT_ADDRESS: string;
+        NEXT_PUBLIC_VOTE_PRICE: string;
+        NEXT_PUBLIC_LOCALSTORAGE_KEY: string;
+        LOCALSTORAGE_KEY: string;
+        NEXTAUTH_URL: string;
+        SLACK_BOT_TOKEN: string;
+        NEXT_PUBLIC_BASE_URL: string;
+        UPLOAD_OUTPUT_API_KEY: string;
+        AWS_BUCKET_NAME: string;
+        AWS_REGION: string;
+        AWS_ACCESS_KEY_ID: string;
+        AWS_SECRET_ACCESS_KEY: string;
+        AWS_S3_URL: string;
+        SLACK_ADMIN_ID: string;
+        SLACK_COMMAND_SELECT_KEY: string;
+    }
+}
