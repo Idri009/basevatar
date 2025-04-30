1e17933f175cf740202da51cc8eca4889e84c00b
enesyukselx <enesyuksel9903@gmail.com>
2025-04-30 13:44:19 +0300

    refactor: update vitest configuration to use uiConfig and add necessary plugins

--- packages/vitest-config/configs/ui-config.ts
+++ packages/vitest-config/configs/ui-config.ts
@@ -1,9 +1,12 @@
-import { defineProject, mergeConfig } from "vitest/config";
 import { baseConfig } from "./base-config.js";
+import { defineProject, mergeConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export const uiConfig = mergeConfig(
     baseConfig,
     defineProject({
+        plugins: [tsconfigPaths(), react()],
         test: {
             environment: "jsdom",
         },
